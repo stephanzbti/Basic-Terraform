8b44c3c4bf811a178a3d29bfb9ae1fd13d499677
Sephan Bartkowiak <stephan@educat.com.br>
2019-12-10 10:33:24 -0300

    Creating Nat-Gateway Module

---
+++ Modules/VPC/Nat-Gateway/variables.tf
@@ -0,0 +1,13 @@
+/*
+    Variables
+*/
+
+variable "tags" {
+    description = "Nat Gateways - Tags"
+    type        = any
+}
+
+variable "gateways" {
+    description = "Nat Gateways - Gateway"
+    type        = any
+}
